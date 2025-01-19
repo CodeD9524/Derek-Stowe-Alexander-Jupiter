db780137fc8c5692dcf73ef93f994aca39a32d72
Derek <amir.stowe95@gmail.com>
2025-01-19 14:42:10 -0500

    lesson11

--- js/index.js
+++ js/index.js
@@ -12,5 +12,30 @@ for (let i = 0; i < skills.length; i++) {
     const skillItem = document.createElement("li");
     skillItem.innerText = skills[i]; 
     skillsList.appendChild(skillItem);
-
 }
+const messageForm = document.getElementsByName("leave_message")[0];
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const usersName = event.target.usersName.value; 
+    const usersEmail = event.target.usersEmail.value; 
+    const usersMessage = event.target.usersMessage.value; 
+    console.log("User's Name:", usersName);
+    console.log("User's Email:", usersEmail);
+    console.log("User's Message:", usersMessage);
+});
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.querySelector("ul");
+const newMessage = document.createElement("li");
+newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+`;
+const removeButton = document.createElement("button");
+removeButton.innerText = "Remove";
+removeButton.type = "button";
+removeButton.addEventListener("click", function() {
+    const entry = removeButton.parentNode;
+    entry.remove();
+});
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage); 
