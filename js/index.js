6e074ba7ea4a1c2d382788638b9d43d4c1ea3fbb
Derek <amir.stowe95@gmail.com>
2025-02-05 17:25:28 -0500

    13

--- js/index.js
+++ js/index.js
@@ -39,3 +39,62 @@ removeButton.addEventListener("click", function() {
 });
 newMessage.appendChild(removeButton);
 messageList.appendChild(newMessage); 
+const url = `https://api.github.com/users/amir95/repos`;
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); 
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+    fetch('https://api.github.com/users/amir95/repos')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const repositories = data;
+    console.log(repositories);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+  fetch(url) 
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    console.log("TESTING",data.id)
+    if (data.length > 0) {
+      console.log('Projects:', data.id);
+    } else {
+      throw new Error('Projects section is empty.');
+    }
+  })
+  .catch(error => {
+    console.error('Fetch error:', error.message);
+    alert(`Error: ${error.message}`);
+  });
+var projectSection = document.getElementById("projects");
+var projectList = projectSection.querySelector("ul");
+var repositories = [
+    { name: "Repository 1" },
+    { name: "Repository 2" },
+    { name: "Repository 3" }
+];
+for (var i = 0; i < repositories.length; i++) {
+    var project = document.createElement("li");
+    project.innerText = repositories[i].name;
+    projectList.appendChild(project);
+}
