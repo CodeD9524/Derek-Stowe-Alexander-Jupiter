4019ffda6245572e167482cbd14fe3a7f30be628
Derek <amir.stowe95@gmail.com>
2025-02-11 10:33:40 -0500

    all

--- js/index.js
+++ js/index.js
@@ -40,33 +40,8 @@ removeButton.addEventListener("click", function() {
 newMessage.appendChild(removeButton);
 messageList.appendChild(newMessage); 
 const url = `https://api.github.com/users/amir95/repos`;
-fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json(); 
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-    fetch('https://api.github.com/users/amir95/repos')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    const repositories = data;
-    console.log(repositories);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+const projectSection = document.getElementById("projects");
+const projectList = projectSection.querySelector("ul");
   fetch(url) 
   .then(response => {
     if (!response.ok) {
@@ -75,26 +50,8 @@ fetch(url)
     return response.json(); 
   })
   .then(data => {
-    console.log("TESTING",data.id)
-    if (data.length > 0) {
-      console.log('Projects:', data.id);
-    } else {
-      throw new Error('Projects section is empty.');
-    }
-  })
-  .catch(error => {
-    console.error('Fetch error:', error.message);
-    alert(`Error: ${error.message}`);
-  });
-var projectSection = document.getElementById("projects");
-var projectList = projectSection.querySelector("ul");
-var repositories = [
-    { name: "Repository 1" },
-    { name: "Repository 2" },
-    { name: "Repository 3" }
-];
-for (var i = 0; i < repositories.length; i++) {
-    var project = document.createElement("li");
-    project.innerText = repositories[i].name;
-    projectList.appendChild(project);
-}
+    data.forEach(repo => {
+        const project = document.createElement("li");
+        project.innerText = repo.name;
+        projectList.appendChild(project);
+    });
